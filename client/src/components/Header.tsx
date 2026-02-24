3ba4133d911cc65c915385a26931ba9b4e303457
nmskm240 <utc.am240@gmail.com>
2026-02-24 06:58:04 +0000

    change faviconの入れかえ

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,13 +2,16 @@ import { AppBar, Box } from "@mui/material";
 
 export function Header() {
   return (
-    <AppBar position="static">
-      <Box
-        component="img"
-        src="/logo.png"
-        alt="Algraph logo"
-        sx={{ width: 120, height: "auto", margin: 1, marginLeft: 2 }}
-      />
+    <AppBar
+      position="sticky"
+        >
+          <Box
+            component="img"
+            src="/logo.svg"
+            alt="Algraph logo"
+            sx={{ width: 120, height: "auto" }}
+          />
+        </Box>
     </AppBar>
   );
 }
